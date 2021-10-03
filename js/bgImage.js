fa264ddd71bfdec35cad5bc162ebb80d968c96b8
antnf3 <antnf3@gmail.com>
2021-10-03 11:09:03 +0900

    Updates

--- js/bgImage.js
+++ js/bgImage.js
@@ -1,5 +1,5 @@
 (() => {
-  const bgImg = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+  const bgImg = ["01.jpg", "02.jpg", "05.jpg"];
   const body = document.querySelector("body");
 
   function getRandum() {
